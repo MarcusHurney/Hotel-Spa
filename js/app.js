f8b35d98d848942ec97cce02d399bb9eb1590c95
Marcus Hurney <marcushurney@gmail.com>
2016-08-28 22:30:45 -0400

    Adds off canvas navigation menu. Adds repsonsive modal to show rooms in the rooms section.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+$(function() {
+  $('#gallery a').on('click', function() {
+    var image = $(this).find('img').attr('src');
+    // in order to change the src route of #galleryModal's img tag
+    // to include the "fullsize" folder instead of "thumbnail"
+    // we use jQuery method split()
+    var selectedImage = image.split('thumbnail');
+    // selectedImage is an array containing the <a> tag's src value split into two values
+    // the first value is before thumbnail and the second is after thumbnail
+    // now we need to manipulate the src to dynamically load the correct photo
+    var fullPath = selectedImage[0] + 'fullsize' + selectedImage[1];
+    // give #galleryModal img tag the correct src value
+    $('#galleryModal #imageContainer').attr('src', fullPath);
+  });
+});
+
+
+
+
 $(document).foundation();
